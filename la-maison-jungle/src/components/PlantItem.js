7cba5b6c9bd30036de4424b96700095856f4eb61
Paul-Louis <paul-louis.renard@outlook.fr>
2022-07-18 05:09:36 +0200

    Add onClick listener

--- la-maison-jungle/src/components/PlantItem.js
+++ la-maison-jungle/src/components/PlantItem.js
@@ -2,7 +2,7 @@ import CareScale from "./CareScale";
 import '../styles/PlantItem.css'
 
 function PlantItem({name, cover, id, light, water, isBestSale}) {
-    return <li key={id} className='lmj-plant-item'>
+    return <li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
         <img src={cover} className='lmj-plant-item-cover' alt={`${name} cover`}/>
         {name} {isBestSale && <span>🔥</span>}
         <div>
@@ -10,6 +10,10 @@ function PlantItem({name, cover, id, light, water, isBestSale}) {
             <CareScale careType='light' scaleValue={light}/>
         </div>
     </li>// && enable to display the text if and only if plant.isBestSale=true
+}
+
+function handleClick(plantName) {
+    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
 }
 
 export default PlantItem
